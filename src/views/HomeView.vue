5fdc4d9e1df652d59d55f506e2d19ac1c2a5bad2
Me <sinon049@yahoo.com.tw>
2022-07-16 18:00:02 +0800

    initial files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>
+    This is HomeView
+  </h1>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => { console.log('This is HomeView')})
+</script>
