1e61560e36a162af97d83fb731d87d46bfafa509
Me <sinon049@yahoo.com.tw>
2022-11-01 17:04:40 +0800

    Modify not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>404 Not Found</h1>
+  <div>
+    <h1>OOPS!</h1>
+    <h5>404 Not Found</h5>
+    <span>Please check your url.</span>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
