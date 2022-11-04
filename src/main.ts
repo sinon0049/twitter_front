9a935c3d368ee5ca31cd547c599deb4923cac0d4
Me <sinon049@yahoo.com.tw>
2022-11-04 15:40:30 +0800

    Add firebase settings

--- src/main.ts
+++ src/main.ts
@@ -51,6 +51,26 @@ dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
 dayjs.locale("zh-tw");
 
+// import env variables
+const env = import.meta.env;
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: env.VITE_APIKEY,
+  authDomain: env.VITE_AUTH_DOMAIN,
+  projectId: env.VITE_PROJECT_ID,
+  storageBucket: env.VITE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_ID,
+  measurementId: env.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const fireBaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireBaseApp);
+
 const app = createApp(App);
 
 app.use(createPinia());
